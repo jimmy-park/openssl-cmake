8edea99c1d125e3c20aebeacd615527209ed9a10
Jiwoo Park <jiwoo_90@naver.com>
2022-12-21 03:18:36 +0900

    Add iOS preset

---
+++ cmake/toolchain/ios.cmake
@@ -0,0 +1,33 @@
+set(CMAKE_SYSTEM_NAME iOS)
+set(CMAKE_OSX_DEPLOYMENT_TARGET 16.0)
+list(APPEND OPENSSL_CONFIGURE_OPTIONS
+    no-tests
+    -fembed-bitcode
+    -fvisibility=hidden
+    -fvisibility-inlines-hidden
+    -fPIC
+)
+
+if(CMAKE_OSX_SYSROOT STREQUAL "iphoneos")
+    set(CMAKE_SYSTEM_PROCESSOR aarch64)
+    set(CMAKE_OSX_ARCHITECTURES arm64)
+    set(OPENSSL_TARGET_PLATFORM ios64-xcrun)
+elseif(CMAKE_OSX_SYSROOT STREQUAL "iphonesimulator")
+    set(CMAKE_OSX_ARCHITECTURES arm64 x86_64)
+    set(CMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH NO)
+    set(OPENSSL_TARGET_PLATFORM iossimulator-xcrun)
+    list(APPEND OPENSSL_CONFIGURE_OPTIONS
+        no-asm
+        "-arch arm64"
+        "-arch x86_64"
+        -mios-simulator-version-min=16.0
+        -fno-common
+        -DL_ENDIAN
+    )
+else()
+    message(FATAL_ERROR "CMAKE_OSX_SYSROOT isn't specified")
+endif()
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
