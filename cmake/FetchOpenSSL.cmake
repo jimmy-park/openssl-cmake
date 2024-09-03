2653886f718a6d19302324d4b105ee4c58753e62
Jiwoo Park <jiwoo_90@naver.com>
2024-09-04 01:31:02 +0900

    Update OpenSSL to 3.3.2 (#20)
    * Update OpenSSL to 3.3.2
    
    * Fix download URL for OpenSSL 1.1.1

--- cmake/FetchOpenSSL.cmake
+++ cmake/FetchOpenSSL.cmake
@@ -1,12 +1,5 @@
 function(fetch_openssl)
-    if("${OPENSSL_SOURCE}" MATCHES "^http")
-        # Download OpenSSL source from the internet
-        CPMAddPackage(
-            NAME openssl-source
-            URL ${OPENSSL_SOURCE}
-            DOWNLOAD_ONLY ON
-        )
-    elseif(EXISTS "${OPENSSL_SOURCE}" AND IS_DIRECTORY "${OPENSSL_SOURCE}")
+    if(EXISTS "${OPENSSL_SOURCE}" AND IS_DIRECTORY "${OPENSSL_SOURCE}")
         # Fetch the local OpenSSL source
         if(NOT IS_ABSOLUTE "${OPENSSL_SOURCE}")
             string(PREPEND OPENSSL_SOURCE ${CMAKE_SOURCE_DIR}/)
@@ -15,23 +8,37 @@ function(fetch_openssl)
         string(REPLACE "\\" "/" openssl-source_SOURCE_DIR "${OPENSSL_SOURCE}")
         set(openssl-source_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl-source-build)
     else()
-        # Download OpenSSL source from the official website
-        if("${OPENSSL_TARGET_VERSION}" STREQUAL "")
-            set(OPENSSL_TARGET_VERSION ${PROJECT_VERSION})
-        endif()
-
-        if(OPENSSL_TARGET_VERSION VERSION_EQUAL PROJECT_VERSION)
-            set(CPM_HASH_OPTION URL_HASH SHA256=777cd596284c883375a2a7a11bf5d2786fc5413255efab20c50d6ffe6d020b7e)
-        else()
-            set(CPM_HASH_OPTION "")
-        endif()
-
-        CPMAddPackage(
+        set(CPM_OPTIONS
             NAME openssl-source
-            URL https://www.openssl.org/source/openssl-${OPENSSL_TARGET_VERSION}.tar.gz
             DOWNLOAD_ONLY ON
-            ${CPM_HASH_OPTION}
         )
+
+        if(NOT OPENSSL_CONFIGURE_VERBOSE)
+            list(APPEND CPM_OPTIONS QUIET)
+        endif()
+
+        if("${OPENSSL_SOURCE}" MATCHES "^http")
+            # Download OpenSSL source from the internet
+            list(APPEND CPM_OPTIONS URL ${OPENSSL_SOURCE})
+        else()
+            # Download OpenSSL source from the official website
+            if("${OPENSSL_TARGET_VERSION}" STREQUAL "")
+                set(OPENSSL_TARGET_VERSION ${PROJECT_VERSION})
+            endif()
+
+            if(OPENSSL_TARGET_VERSION VERSION_EQUAL PROJECT_VERSION)
+                list(APPEND CPM_OPTIONS URL_HASH SHA256=2e8a40b01979afe8be0bbfb3de5dc1c6709fedb46d6c89c10da114ab5fc3d281)
+            endif()
+
+            if(OPENSSL_TARGET_VERSION MATCHES "^1\.1\.1[a-w]$")
+                string(REPLACE "." "_" OPENSSL_TAGGED_VERSION ${OPENSSL_TARGET_VERSION})
+                list(APPEND CPM_OPTIONS URL https://github.com/openssl/openssl/releases/download/OpenSSL_${OPENSSL_TAGGED_VERSION}/openssl-${OPENSSL_TARGET_VERSION}.tar.gz)
+            else()
+                list(APPEND CPM_OPTIONS URL https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_TARGET_VERSION}/openssl-${OPENSSL_TARGET_VERSION}.tar.gz)
+            endif()
+        endif()
+
+        CPMAddPackage(${CPM_OPTIONS})
     endif()
 
     # Clean build directory if source directory has changed
